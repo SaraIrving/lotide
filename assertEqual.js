de0d4d7aaf1b4656cccae901704e80f5eef0bafa
Sara Irving <sarakirving@gmail.com>
2020-06-23 02:30:52 +0000

    Creating your own assert function to compare two inputs

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("sara", "sarah");
+assertEqual("cow", "cow");
+assertEqual(3, 9);
+
