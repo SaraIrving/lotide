aff31e74f64881e10c947d61bc02ec7f82a6a3c9
Sara Irving <sarakirving@gmail.com>
2020-06-24 22:06:45 +0000

    A function to count only certain items in and array and then return an onbejct with all the present items and their respective counts

---
+++ countOnly.js
@@ -0,0 +1,52 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//countOnly will be given an array and an object. 
+//It will return an object containing counts 
+//of everything that the input object listed.
+//Only keys which have a truthy value should be 
+    //counted in the resulting object
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
+
+
+//TESTS:
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
