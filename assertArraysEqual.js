22db9e12e318c463375cf25fae15bc823f96e746
Sara Irving <sarakirving@gmail.com>
2020-06-24 01:18:29 +0000

    A function to compare two arrays and determine if they are identical

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+function eqArrays (array1, array2) {
+  //check that arrays are the same length
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  //if they are the same length, now compare the elements
+  //to be sure they match
+  for (let i = 0; i < array1.length; i++){
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+function assertArraysEquals (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return console.log(`👍👍👍Array Assertion Passed: ${array1} equals ${array2}`);
+  } else {
+    return console.log(`🛑🛑🛑Array Assertion Failed: ${array1} does not equal ${array2}`);
+  }
+}
+
+//TESTS:
+assertArraysEquals([1,2,3,4], [1,2,3,4]); //true
+assertArraysEquals(["2", "4"], ["2", "4"]); //true
+assertArraysEquals([1,2,3,4], [9,8,7,6]); //false
+assertArraysEquals(["the", "end"], ["the", "start"]); //false
