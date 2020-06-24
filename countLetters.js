07cf1e202adc725d1bfe856799b81d3ce76353f7
Sara Irving <sarakirving@gmail.com>
2020-06-24 22:56:19 +0000

    A function to count how many of each letter are in a string, returns an object that contains each letter and it's count

---
+++ countLetters.js
@@ -0,0 +1,61 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//The function should take in a sentence (as a string) 
+//and then return a count of each of 
+//the letters in that sentence.
+// -->skip and don't count any spaces in the string
+
+/*
+Pseudocode:
+-loop over the characters in the string
+  --ignore the spaces
+  --only count each character once
+  --assign each character as a key value and assign it a count
+  --if the key already exists, increase the count 
+  --make letters all lower case so it does not impact results 
+-return the object 
+
+*/
+
+function countLetters(inputString) {
+  let countedLetters = [];
+  let lowerCaseString = inputString.toLowerCase();
+  for (let char of lowerCaseString) {
+    if (char !== " ") {
+      if (char in countedLetters){
+        countedLetters[char] += 1;
+      } else {
+        countedLetters[char] = 1;
+      }
+    }
+
+  }
+  return countedLetters;
+}
+
+
+//TESTS:
+countLetters("lighthouse in the house"); 
+/*{
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+}
+*/
+
+countLetters("Sarasss");
+countLetters("Sara Irving"); 
