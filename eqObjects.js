09ffac82323d469a8c17b55cf5937166ae58b46e
Sara Irving <sarakirving@gmail.com>
2020-06-25 03:22:12 +0000

    Removed one of the else statements

--- eqObjects.js
+++ eqObjects.js
@@ -25,20 +25,20 @@ function eqObjects(object1, object2) {
   let keys2 = Object.keys(object2);
   if (keys1.length !== keys2.length) {
     return false;
-  } else {
-    for (let key of keys1) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if(!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else {
-        if (object1[key] !== object2[key]) {
-          return false;
-        }  
-      }
-    }
-    return true;
   }
+
+  for (let key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if(!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }  
+    }
+  }
+  return true;
 } 
 
 
