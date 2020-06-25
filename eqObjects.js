44b44993852ec2ceb600f6bc73a22b9ea73506ef
Sara Irving <sarakirving@gmail.com>
2020-06-25 03:18:43 +0000

    console log tests removed

--- eqObjects.js
+++ eqObjects.js
@@ -8,12 +8,9 @@ const assertEqual = function(actual, expected) {
 
 
 function eqArrays (array1, array2) {
-  //check that arrays are the same length
   if (array1.length !== array2.length) {
     return false;
   }
-  //if they are the same length, now compare the elements
-  //to be sure they match
   for (let i = 0; i < array1.length; i++){
     if (array1[i] !== array2[i]) {
       return false;
@@ -23,25 +20,14 @@ function eqArrays (array1, array2) {
 }
 
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 function eqObjects(object1, object2) {
   let keys1 = Object.keys(object1);
-  //console.log("keys1=", keys1, "k1 length= ", keys1.length);
   let keys2 = Object.keys(object2);
-  //console.log("keys2=", keys2, "k2 length= ", keys2.length);
   if (keys1.length !== keys2.length) {
     return false;
   } else {
     for (let key of keys1) {
-      //console.log("key = ", key);
-      //console.log("object1[key]= ", object1[key]);
-      //console.log("object2[key]= ", object2[key]);
-    //if the values don't match what do you do?
-      //console.log('Array.isArray(object1[key])=', Array.isArray(object1[key]));
-      //console.log('Array.isArray(object2[key])=', Array.isArray(object2[key]));
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {//check to see if both values are arrays, if they are check to see if they are equal 
-        //console.log("result of eqArray()= ", eqArrays(object1[key], object2[key]));
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if(!eqArrays(object1[key], object2[key])) {
           return false;
         }
