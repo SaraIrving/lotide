2f2a0afcab2d3ad4c514166d3a20707df8aa0e9e
Sara Irving <sarakirving@gmail.com>
2020-06-25 02:00:27 +0000

    a function to find a key given it's value

---
+++ findKeyByValue.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function findKeyByValue(object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+
+
+
+
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const bestFoodByRegion = {
+  thai: "Yellow Curry",
+  japanese: "Ramen",
+  mexican: "Tacos"
+};
+
+assertEqual(findKeyByValue(bestFoodByRegion, "Yellow Curry"), "thai");
+assertEqual(findKeyByValue(bestFoodByRegion, "Ramen"), "japanese");
+assertEqual(findKeyByValue(bestFoodByRegion, "Tater tots"), undefined);
