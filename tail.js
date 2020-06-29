70c606547b62a538c999f1363bb8ff4e59b0d0ad
Sara Irving <sarakirving@gmail.com>
2020-06-29 20:23:06 +0000

    Cleaned up this function, added module.exports for the tail function, removed manual tests

--- tail.js
+++ tail.js
@@ -1,34 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const tail = function(arrayInput) {
   return arrayInput.slice(1);
 };
 
-
-
-
-
-
-
-
-//TESTS:
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const empty = tail([]);
-assertEqual(empty, []);
-
-const single = tail([2]);
-assertEqual(single.lenth, undefined);
+module.exports = tail;
