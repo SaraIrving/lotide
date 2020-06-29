81e36fb2189c112646600fb4e6fc933aa1d12ba5
Sara Irving <sarakirving@gmail.com>
2020-06-29 22:09:01 +0000

    Refactored code to test using Mocha and Chai, restructured tests to use the describle/it block syntax

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,26 @@
 
-const assertArraysEqual = require("../assertArraysEqual");
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3]), [2]) 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) 
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]) 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]) 
-assertArraysEqual(middle([1]), []) 
-assertArraysEqual(middle([1, 2]), []) 
+describe("#middle", () => {
+  it('Should return [2] when given [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('Should return [3] when given [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it('Should return [3, 4] when given [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it('Should return [] when given [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it('Should return [] when given [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
+
