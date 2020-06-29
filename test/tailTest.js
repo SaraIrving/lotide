4a6d9df5ab95a105e4e9312380b18ca0c508acb6
Sara Irving <sarakirving@gmail.com>
2020-06-29 22:02:49 +0000

    refactored to use Chai and Mocha, removed previous tests, wrote tests in describe/it format instead

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,19 @@
-const assertEqual = require("../assertEqual");
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+  it('Should return ["Lighthouse", "Labs"] when given ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
 
-//TESTS:
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  it('Should return [] when given "[]"', () => {
+    assert.deepEqual(tail([]), []);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it('Should return undefined when given ["2"]', () => {
+    assert.deepEqual(tail(["2"]), []);
+  });
 
-const empty = tail([]);
-assertEqual(empty, []);
+});
 
-const single = tail([2]);
-assertEqual(single.length, undefined);
