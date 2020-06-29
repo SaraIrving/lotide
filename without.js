6dc751a00fa4bd152751273937e9861e43beb996
Sara Irving <sarakirving@gmail.com>
2020-06-29 23:01:18 +0000

    commented out the manual tests

--- without.js
+++ without.js
@@ -42,10 +42,10 @@ function without(source, itemsToRemove) {
 
 module.exports = without;
 
-console.log("without returns: " , without([1, 2, 3], [1])); // => [2, 3]
-console.log("without returns: ", without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-console.log("without returns: ", without(["1", "2", "3"], [1, 2, 3])); // => ["1", "2", "3"]
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// console.log("without returns: " , without([1, 2, 3], [1])); // => [2, 3]
+// console.log("without returns: ", without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+// console.log("without returns: ", without(["1", "2", "3"], [1, 2, 3])); // => ["1", "2", "3"]
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
