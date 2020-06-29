2ac1ebcd9f85b615686a411ce75ec6b4e7a29d87
Sara Irving <sarakirving@gmail.com>
2020-06-29 20:10:23 +0000

    This file tests the code for assertEquals function

---
+++ test/assertEqualTest.js
@@ -0,0 +1,14 @@
+const assertEqual = require('../assertEqual');
+
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp"); //false
+assertEqual(1, 1); //true
+assertEqual("sara", "sarah"); //false
+assertEqual("cow", "cow");//true
+assertEqual(3, 9);//false
+
+
+
+
