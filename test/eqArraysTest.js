26135af75b30a8ef4fcce4188738a2c4b191e89e
Sara Irving <sarakirving@gmail.com>
2020-06-30 03:36:40 +0000

    Updated manual tests code

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,7 +2,6 @@ const eqArrays = require("../eqArrays");
 const assertEqual = require("../assertEqual");
 
 
-
 //TESTS:
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
 eqArrays([1, 2, 3], [3, 2, 1]) // => false
@@ -11,7 +10,7 @@ eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should Fail
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false); //false 
