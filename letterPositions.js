c9d92187c8143fc69d1475a676394133325aa7f7
Sara Irving <sarakirving@gmail.com>
2020-06-30 05:49:40 +0000

    removed manual tests

--- letterPositions.js
+++ letterPositions.js
@@ -1,29 +1,4 @@
 
-function eqArrays (array1, array2) {
-  //check that arrays are the same length
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  //if they are the same length, now compare the elements
-  //to be sure they match
-  for (let i = 0; i < array1.length; i++){
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-
-function assertArraysEqual (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    return console.log(`👍👍👍Array Assertion Passed: ${array1} equals ${array2}`);
-  } else {
-    return console.log(`🛑🛑🛑Array Assertion Failed: ${array1} does not equal ${array2}`);
-  }
-}
-
-
 const letterPositions = function(sentence) {
   const results = {};
   sentence = sentence.toLowerCase(); 
@@ -41,14 +16,3 @@ const letterPositions = function(sentence) {
 };
 
 module.exports = letterPositions;
-
-
-
-// //TESTS:
-// //console.log(letterPositions("Sara"));
-// //console.log(letterPositions("lighthouse in the house"));
-
-// assertArraysEqual(letterPositions("hello").e, [1]);
-// assertArraysEqual(letterPositions("Sara").a, [1, 3]);
-// assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
-
