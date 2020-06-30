6e38e722f225bed16db4b05a22606391a97203bc
Sara Irving <sarakirving@gmail.com>
2020-06-30 00:16:06 +0000

    modified

--- countLetters.js
+++ countLetters.js
@@ -43,22 +43,22 @@ function countLetters(inputString) {
 
 module.exports = countLetters;
 
-//TESTS:
-//countLetters("lighthouse in the house"); 
-/*{
-  l: 1,
-  i: 2,
-  g: 1,
-  h: 4,
-  t: 2,
-  o: 2,
-  u: 2,
-  s: 2,
-  e: 3,
-  n: 1,
-}
-*/
+// //TESTS:
+// //countLetters("lighthouse in the house"); 
+// /*{
+//   l: 1,
+//   i: 2,
+//   g: 1,
+//   h: 4,
+//   t: 2,
+//   o: 2,
+//   u: 2,
+//   s: 2,
+//   e: 3,
+//   n: 1,
+// }
+// */
 
-countLetters("Sarasss");  // { s: 4, a: 2, r: 1 }
-countLetters("Sara Irving"); // { s: 1, a: 2, r: 2, i: 2, v: 1, n: 1, g: 1 });
+// countLetters("Sarasss");  // { s: 4, a: 2, r: 1 }
+// countLetters("Sara Irving"); // { s: 1, a: 2, r: 2, i: 2, v: 1, n: 1, g: 1 });
 
