3da788a4a6cef17d75c19cf1b52cc4b3c37d6200
Sara Irving <sarakirving@gmail.com>
2020-06-30 03:32:03 +0000

    Updated manual test code

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,7 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
-
-assertArraysEqual([1,2,3,4], [1,2,3,4]); 
-assertArraysEqual(["2", "4"], ["2", "4"]); 
-assertArraysEqual([1,2,3,4], [9,8,7,6]); 
-assertArraysEqual(["the", "end"], ["the", "start"]); 
+//TESTS:
+assertArraysEqual([1,2,3,4], [1,2,3,4]); //true
+assertArraysEqual(["2", "4"], ["2", "4"]); //true
+assertArraysEqual([1,2,3,4], [9,8,7,6]); //false
+assertArraysEqual(["the", "end"], ["the", "start"]); //false
