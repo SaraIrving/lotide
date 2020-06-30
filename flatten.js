819ea8e5e3a7143f0309bc4fc0f0ca942346d86a
Sara Irving <sarakirving@gmail.com>
2020-06-30 03:44:37 +0000

    removed manual tests and calls to eqArrays and assertArrays Equal

--- flatten.js
+++ flatten.js
@@ -1,39 +1,12 @@
-function eqArrays (array1, array2) {
-  //check that arrays are the same length
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  //if they are the same length, now compare the elements
-  //to be sure they match
-  for (let i = 0; i < array1.length; i++){
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
 
-
-function assertArraysEqual (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    return console.log(`👍👍👍Array Assertion Passed: ${array1} equals ${array2}`);
-  } else {
-    return console.log(`🛑🛑🛑Array Assertion Failed: ${array1} does not equal ${array2}`);
-  }
-}
-
-
-//take in a nested array, retun a flattened array 
 function flatten (array) {
   let flatArray = [];
   for (let element of array) {
     if (Array.isArray(element)) {
-      //loop through array and push elements to flatArray
       for (let item of element) {
         flatArray.push(item);
       }
     } else {
-      //push element to flatArray
       flatArray.push(element);
     }
   }
@@ -42,5 +15,3 @@ function flatten (array) {
 
 module.exports = flatten;
 
-// //TESTS:
-// console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
