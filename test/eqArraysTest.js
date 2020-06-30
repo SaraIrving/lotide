2819dfe2bb7a8f8af50a7dadc69313d2c8ebe23d
Sara Irving <sarakirving@gmail.com>
2020-06-30 06:00:58 +0000

    created unit tests for eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,16 +1,12 @@
 const eqArrays = require("../eqArrays");
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 
+describe("#eqArrays", () => {
+  it("should return true when given [1, 2, 3], [1, 2, 3]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
 
-//TESTS:
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should Fail
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false); //false 
+  it("should return false when given ['1', '2', '3'], ['1', '2', 3]", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  })
+})
